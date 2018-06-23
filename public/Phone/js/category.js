68fc5c7f5762ea441c3e19cb0f2e45b43f48d695
Xiaoxi <1135022744@qq.com>
2018-06-23 21:09:22 +0800

    电商全端—_移动端分类页面完成

---
+++ public/Phone/js/category.js
@@ -0,0 +1,50 @@
+$(function () {
+
+  //获取分类名（一级分类）数据，并动态渲染
+  $.ajax ({
+
+    type: 'get',
+    url: '/category/queryTopCategory',
+    success: function (info) {
+      // console.log(info);
+      $(".category_left ul").html(template("tpl1", info));
+
+      //获取到一级分类之后，渲染了第一个一级分类对应的二级分类
+      renderSecond( info.rows[0].id );
+    }
+
+  });
+
+  //点击一级分类名，动态渲染右边二级分类logo
+  $(".category_left").on("click", "li", function () {
+
+    //给点击的li加now类并排他
+    $(this).addClass("now").siblings().removeClass("now");
+
+    //渲染点击选中的li（一级分类名）的所有二级分类logo
+    var id = $(this).data("id");
+    renderSecond(id);
+
+  });
+
+
+  //二级分类渲染封装
+  function renderSecond(id) {
+
+    $.ajax ({
+
+      type: 'get',
+      url: '/category/querySecondCategory',
+      data: {
+        id: id,
+      },
+      success: function (info) {
+        // console.log(info);
+        $(".category_right ul").html(template("tpl2", info));
+      }
+
+    });
+
+  }
+
+});
