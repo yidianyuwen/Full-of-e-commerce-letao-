38cde12e8576a2300021483c89354c7519aad715
Xiaoxi <1135022744@qq.com>
2018-06-25 19:46:26 +0800

    给所有PC端页面加顶部加载进度条效果

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -1,19 +1,19 @@
 //进度条功能
 //禁用进度环
-// NProgress.configure({ showSpinner: false });
+NProgress.configure({ showSpinner: false });
 
-// //注册一个全局的ajaxStart事件，所有的ajax在开启的时候，会触发这个事件
-// $(document).ajaxStart(function () {
-//   //开启进度条
-//   NProgress.start();
-// });
+//注册一个全局的ajaxStart事件，所有的ajax在开启的时候，会触发这个事件
+$(document).ajaxStart(function () {
+  //开启进度条
+  NProgress.start();
+});
 
-// $(document).ajaxStop(function () {
-//   //完成进度条
-//   setTimeout(function () {
-//     NProgress.done();
-//   }, 500);
-// });
+$(document).ajaxStop(function () {
+  //完成进度条
+  setTimeout(function () {
+    NProgress.done();
+  }, 500);
+});
 
 //非登陆页面，判断当前用户是否是登录了，如果登录了，就继续，如果没登陆，需要跳转到登录页面。
 //后端已经设置好此功能，这种是前段实现判断跳转的功能
